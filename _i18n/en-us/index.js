5ab22f03a2147d9ef3593eca3c88f6edccae2ae0
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-25 17:11:39 -0500

    + added translations

---
+++ _i18n/en-us/index.js
@@ -0,0 +1,26 @@
+export default {
+  "igamification": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Gamification",
+        "adminActivities": "Activities",
+        "adminCategories": "Categories"
+      },
+      "newActivity": "New Activity",
+      "updateActivity": "Update Activity",
+      "newCategory": "New Category",
+      "updateCategory": "Update Category",
+      "label": {
+        "tourElement": "Element Id",
+        "tourElementPosition": "Element Position"
+      },
+      "activities": {
+        "repeatAction": "Tour",
+        "repeatActionDescription": "From here you can repeat this tour whenever you want"
+      },
+      "documentation": {
+        "categories": "The gamification module is used to improve the user experience in the application, With this functionality it is possible to create tours, popups, or specific activities per module. It is possible to create a category that current as the grouping element and assigned to this one or several activities that the user will be available to view."
+      }
+    }
+  }
+}
---
+++ _i18n/es/index.js
@@ -0,0 +1,26 @@
+export default {
+  "igamification": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Gamificación",
+        "adminActivities": "Actividades",
+        "adminCategories": "Categorias"
+      },
+      "newActivity": "Nueva Actividad",
+      "updateActivity": "Actualizar Actividad",
+      "newCategory": "Nueva Categoría",
+      "updateCategory": "Actualizar Categoría",
+      "label": {
+        "tourElement": "ID del elemento",
+        "tourElementPosition": "Pocisión del Elemento"
+      },
+      "activities": {
+        "repeatAction": "Tour",
+        "repeatActionDescription": "Desde aquí puedes repetir este tour cuando quieras"
+      },
+      "documentation": {
+        "categories": "El modulo de gamificación se usa para mejorar la experiencia del usuario en la aplicación, con está funcionalidad es posible crear tours, popups, o actividades especificas por modulo. Es posible crear una categoría que actual como el elemento agrupador y asignado a esta una o varias actividades que el usuario tendrá disponible para ver."
+      }
+    }
+  }
+}
